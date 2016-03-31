2fe7b72617058a123a8fdf2eb6e837707acff3d4
VLKxCMIT <VLKxADM@gmail.com>
2016-03-31 09:23:46 +0300

    js fix on page load

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-var firstHeight = document.getElementById('slide-1').offsetHeight;
-document.getElementById('slideViewer').style.height = firstHeight + 'px';
+window.onload=function() {
+	var inputs = document.getElementsByName("toggle");
+	var selectedValue = -1;
+	var input_len = inputs.length;
+	for (var i = 0; i < input_len; i++) {
+	    if (inputs[i].checked)
+	    {
+	    	var valueI = i + 1;
+	        selectedValue = 'slide-' + valueI;
+	        var firstHeight = document.getElementById(selectedValue).offsetHeight;
+			document.getElementById('slideViewer').style.height = firstHeight + 'px';
+	        break;
+	    }
+	}
+}
 
 function sliderHeight(sl) {
 	var offsetHeight = document.getElementById(sl).offsetHeight;
